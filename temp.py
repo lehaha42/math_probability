532771f0ef93da96bd4bec5b42c16ce6beaefa3b
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2025-02-17 15:07:46 +1000

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 
 
 def main():
-    for i in range(13,18):
+    for i in range(0):
         open(f'laba1/t{i}.py', 'w').write('\n\ndef main():\n\tprint()\n\n\nif __name__ == "__main__":\n\tmain()\n')
 
 
