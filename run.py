aecbe2c446de006c41659ad87e719e2f84d0873e
lehaha42 <lehaha42@edu.vvsu.ru>
2025-03-24 14:50:31 +1000

    update

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 
 
-def main(n = -1):
+def main(n = 0):
     arr = [16, 7, 16, 10]
-    for j in range(len(arr)) if (n >= len(arr) or n < 0) else [n]:
+    for j in range(len(arr)) if (n > len(arr) or n < 1) else [n-1]:
         for i in range(arr[j]):
             print(f'laba {j+1} task {i+1}:')
             exec(f'import laba{j+1}.t{i+1}\nlaba{j+1}.t{i+1}.main()')
@@ -10,4 +10,4 @@ def main(n = -1):
 
 
 if __name__ == '__main__':
-    main()
+    main(4)
