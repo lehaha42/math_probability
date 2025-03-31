6f3ed75d0e558e883524e0babe17832a379e0d76
lehaha42 <lehaha42@edu.vvsu.ru>
2025-03-31 13:46:06 +1000

    update

--- laba4/t1.py
+++ laba4/t1.py
@@ -1,8 +1,9 @@
 from formulas import C
 
+
 def main():
 	for i in range(7):
-		print(C(i, 6)*C(6-i, 39)/C(6, 45))
+		print(f'P(x={i}) = {C(i, 6)*C(6-i, 39)/C(6, 45)}')
 
 
 if __name__ == "__main__":
