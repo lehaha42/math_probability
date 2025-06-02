92fa37c17e502f500b7c1df933fd223400fe8528
lehaha42 <lehaha42@edu.vvsu.ru>
2025-06-02 13:12:00 +1000

    update

--- laba10/t3.py
+++ laba10/t3.py
@@ -1,24 +1,32 @@
 
-ACC = 1e-8
+ACC = 1e-9
 
 def f(y, x):
 	return (y(x+ACC/2) - y(x-ACC/2))/ACC
 
 
 def main():
-	y1 = lambda x: .62 + 58.74/x
-	y2 = lambda x: 9.3 + 9.83*x
-	y3 = lambda x: 11.75 * x**1.6281
-	y4 = lambda x: 14.87 * 1.016**x
 	M, N, K = 1.2, 6, 2.1
-	x = 2.64 + M
-	print(f'Э(y1) = {f(y1, x) * x / y1(x)}')
-	x = 1.38 + N
-	print(f'Э(y2) = {f(y2, x) * x / y2(x)}')
-	x = 1.503 + K
-	print(f'Э(y3) = {f(y3, x) * x / y3(x)}')
-	x = 26.3 + N
-	print(f'Э(y4) = {f(y4, x) * x / y4(x)}')
+	y = [
+		lambda x: .62 + 58.74/x,
+		lambda x: 9.3 + 9.83*x,
+		lambda x: 11.75 * x**1.6281,
+		lambda x: 14.87 * 1.016**x
+	]
+	x = [
+		2.64 + M,
+		1.38 + N,
+		1.503 + K,
+		26.3 + N
+	]
+	a = list(map(lambda y, x: f(y, x) * x / y(x), y, x))
+	b = a.copy()
+	b.sort()
+	for i in range(4):
+		for j in range(4):
+			if a[i] == b[j]:
+				print(f"Э(y{i+1}) = {a[i]}\t\t{['weak', 'mid', 'strong', 'very strong'][j]}")
+				break
 
 
 if __name__ == "__main__":
